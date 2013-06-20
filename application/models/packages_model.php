27a602bef2b495a306f001389f8b8cb1587075ee
M Shafiq Islam <shafiq@emicrograph.com>
2013-06-20 11:29:49 +0600

    Merge branch 'imrandevelop' into testmerge
    Conflicts:
    	.idea/modules.xml
    	application/controllers/api/cakes.php
    	application/models/categories_model.php
    	application/models/customers_model.php
    	application/models/locations_model.php
    	application/models/users_model.php
    	application/models/zones_model.php

--- application/models/packages_model.php
+++ application/models/packages_model.php
@@ -12,7 +12,7 @@ class Packages_model extends Ci_Model
 
     public function getAll(){
 
-        $sql = ("SELECT *
+        $sql = ("SELECT price_matrix.price_matrix_id,price_matrix.price,servings.title AS servings_title , servings.size,flavours.*
             FROM price_matrix
             LEFT JOIN servings ON price_matrix.serving_id = servings.serving_id
             LEFT JOIN flavours ON price_matrix.flavour_id = flavours.flavour_id"
@@ -34,7 +34,7 @@ class Packages_model extends Ci_Model
             $output[$flavours[$flavour_id]]['fondant'] = (int) $val['fondant'];
             $count = isset($output[$flavours[$flavour_id]]["servings"]) ? count($output[$flavours[$flavour_id]]["servings"]) : 0;
             $output[$flavours[$flavour_id]]["servings"][$count]['serving_id'] = (int) $val['price_matrix_id'];
-            $output[$flavours[$flavour_id]]["servings"][$count]['title'] = $val['title'];
+            $output[$flavours[$flavour_id]]["servings"][$count]['servings_title'] = $val['servings_title'];
             $output[$flavours[$flavour_id]]["servings"][$count]['size'] = $val['size'];
             $output[$flavours[$flavour_id]]["servings"][$count]['price'] =(float) $val['price'];
         }
