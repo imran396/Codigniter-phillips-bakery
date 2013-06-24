8a552703412db4b3e34c505e97428fd91480bce8
M Shafiq Islam <shafiq@emicrograph.com>
2013-06-24 17:37:11 +0600

    Merge branch 'imrandevelop' into testmerge
    Conflicts:
    	.idea/modules.xml
    	application/controllers/api/cakes.php
    	application/models/categories_model.php
    	application/models/customers_model.php
    	application/models/locations_model.php
    	application/models/users_model.php
    	application/models/zones_model.php

---
+++ application/models/orders_model.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Orders_model extends Ci_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+
+    public function getOrderStatus(){
+
+
+
+         //return $output;
+
+
+    }
+
+
+}
