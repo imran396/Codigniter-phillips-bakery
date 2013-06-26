08208e43c990696caf0cc22fd94269b44141a9b5
M Shafiq Islam <shafiq@emicrograph.com>
2013-06-26 15:40:11 +0600

    Merge branch 'imrandevelop' into testmerge
    Conflicts:
    	.idea/modules.xml
    	application/controllers/api/cakes.php
    	application/models/categories_model.php
    	application/models/customers_model.php
    	application/models/locations_model.php
    	application/models/users_model.php
    	application/models/zones_model.php

--- application/models/packages_model.php
+++ application/models/packages_model.php
@@ -34,7 +34,7 @@ class Packages_model extends Ci_Model
                 $output[$flavours[$flavour_id]]['flavour_title'] = $val['title'];
                 $output[$flavours[$flavour_id]]['fondant'] = (int) $val['fondant'];
                 $count = isset($output[$flavours[$flavour_id]]["servings"]) ? count($output[$flavours[$flavour_id]]["servings"]) : 0;
-                $output[$flavours[$flavour_id]]["price_matrix_id"][$count]['price_matrix_id'] = (int) $val['price_matrix_id'];
+                $output[$flavours[$flavour_id]]["servings"][$count]['price_matrix_id'] = (int) $val['price_matrix_id'];
                 $output[$flavours[$flavour_id]]["servings"][$count]['title'] = $val['servings_title'];
                 $output[$flavours[$flavour_id]]["servings"][$count]['size'] = $val['size'];
                 $output[$flavours[$flavour_id]]["servings"][$count]['price'] = $price;
