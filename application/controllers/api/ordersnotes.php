bbb16937e11f801a7b4ebe32f0c9757ecc483c37
imran <imran3968@gmail.com>
2013-07-09 17:01:16 +0600

    ordernotes api

---
+++ application/controllers/api/ordersnotes.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once __DIR__ . '/API_Controller.php';
+
+class Ordersnotes extends API_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('orders_model');
+    }
+
+
+    public function notes($order_id){
+        $data = $this->orders_model->getAllNotes($order_id);
+        $this->sendOutput($data);
+
+    }
+
+    public function save(){
+        if(!empty($_POST)){
+            $this->orders_model->SaveNotes($_POST);
+            if($this->db->insert_id()){
+                $data = array('message'=>'created successfull','code'=>'201');
+                $this->sendOutput($data);
+            }
+        }
+    }
+
+    public function search(){
+        $request = $this->input->get();
+        if($request){
+        $data = $this->orders_model->NotesSearch($request);
+        $this->sendOutput($data);
+        }
+
+    }
+
+}
