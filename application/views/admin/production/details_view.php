e4be00bddb83e57931fb61dc2577eedf44bb8eae
M Shafiq Islam <shafiq@emicrograph.com>
2013-07-15 16:12:48 +0600

    Print css add

--- application/views/admin/production/details_view.php
+++ application/views/admin/production/details_view.php
@@ -78,7 +78,7 @@
                 <ul>
                     <li>
                         <span>Total</span>
-                        <span class="count"><?php if($queryup->override_price !='0.00') echo $queryup->override_price; else echo $queryup->total_price; ?></span>
+                        <span class="count">$<?php if($queryup->override_price !='0.00') echo $queryup->override_price; else echo $queryup->total_price; ?></span>
                     </li>
                 </ul>
             </div>
