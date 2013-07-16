eb0891f64f9c42199927ef9980d47beff9db3c75
imran <imran3968@gmail.com>
2013-07-16 12:01:29 +0600

    email template

---
+++ application/views/email/estimate_view.php
@@ -0,0 +1,227 @@
+<style type="text/css">
+    *
+    {
+        margin:0;
+        padding:0;
+        line-height:16px;
+        font-size:12px;
+        font-family:Times;
+    }
+    .clr
+    {
+        clear:both;
+    }
+    .cen
+    {
+        text-align:center;
+    }
+    .left
+    {
+        float:left;
+    }
+    .right
+    {
+        float:right;
+    }
+    .align-right
+    {
+        text-align:right;
+    }
+    .col_half
+    {
+        width:50%;
+    }
+    .col_fourth
+    {
+        width:25%;
+    }
+    #wrapper
+    {
+        width:700px;
+        margin:0 auto;
+    }
+    ul{
+        list-style-type: none;
+    }
+    h1
+    {
+        font-size:32px;
+        line-height:40px;
+    }
+</style>
+
+<div id="wrapper">
+    <div class="col_half left">
+        <h1>ESTIMATE</h1>
+    </div>
+
+    <div class="col_half right">
+        <p class="align-right">St. Phillip's Bakery</p>
+
+        <p class="align-right">2563 Major Mackenzie Drive</p>
+        <p class="align-right">Maple, ON L6A 2E8</p>
+        <p class="align-right">info@stphillipsbakery.com</p>
+        <p class="align-right">905-832-5688</p>
+    </div>
+    <div class="clr"></div>
+
+    <p>ORDER #19043823 01/01/2013</p>
+    <hr />
+    <br />
+    <ul class="col_fourth left">
+        <li>CUSTOMER DETAILS</li>
+    </ul>
+
+    <ul class="col_fourth left">
+        <li>ORDER INFORMATION</li>
+    </ul>
+
+    <ul class="col_fourth left">
+        <li>&nbsp;</li>
+    </ul>
+
+    <ul class="col_fourth left">
+        <li>DELIVER TO</li>
+    </ul>
+
+    <div class="clr"></div>
+    <br />
+
+
+
+
+
+    <ul class="col_fourth left">
+        <li>Spencer Spear</li>
+        <li>123 Fake St</li>
+        <li>Unit 678</li>
+        <li>Toronto, ON A1A 1A1</li>
+        <li>905-832-5688</li>
+        <li>spencer@gsisolutions.ca</li>
+    </ul>
+    <div class="left col_fourth">
+        <ul class="">
+            <li>PICKUP/DELIVERY:</li>
+            <li>DATE:</li>
+            <li>PICKUP LOCATION:</li>
+            <li>DELIVERY ZONE:</li>
+        </ul>
+    </div>
+
+    <ul class="left col_fourth">
+        <li>Pickup</li>
+        <li>01/01/2013 13:00</li>
+        <li>2563 Major Mackenzie Drive</li>
+        <li>Zone 1</li>
+    </ul>
+
+    <ul class="left col_fourth">
+
+        <li>Hall manager - Mike</li>
+        <li>234 Fake St</li>
+        <li>Unit 678</li>
+        <li>Toronto, ON A1A 1A1</li>
+        <li>905-832-5688</li>
+        <li>mike@yourhall.com</li>
+    </ul>
+    <div class="clr"></div>
+    <br />
+    <hr />
+    <br />
+    <p>CAKE DETAILS</p>
+    <div class="clr"></div>
+    <br />
+
+    <ul class="col_half left">
+        <li>Image on cake: image_4.png</li>
+    </ul>
+
+    <ul class="col_half left">
+        <li>Reference Images: image_1.png, image_2.png, image_3.png</li>
+    </ul>
+    <div class="clr"></div>
+    <br />
+    <ul class="col_half left">
+        <li>Inscription: </li>
+        <br />
+        <li>> happy Birthday</li>
+    </ul>
+    <ul class="col_half left">
+        <li>Special Instructions:</li>
+        <br />
+        <li>> Please make sure the cake has 3 layers</li>
+        <li>> and a yellow fondant with purple flowers</li>
+    </ul>
+    <div class="clr"></div>
+    <br />
+    <hr />
+    <br />
+
+    <div class="col_half left">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p class="cen">
+            <img src="http://gsipreview.net/stp-templates/img/barcode.jpg" width="60%"\\/>
+        </p>
+        <br />
+        <p class="cen">Thank You</p>
+        <p class="cen">info@stphillipsbakery.com</p>
+        <p class="cen">stphillipsbakery.com</p>
+
+    </div>
+
+
+    <div class="col_half right">
+        <ul class="col_half left">
+            <li>Birthday Cake</li>
+            <li>MAGIC CAKE ID:</li>
+            <li>FLAVOUR:</li>
+            <li>SIZE:</li>
+            <li>SHAPE:</li>
+            <li>SERVING:</li>
+            <li>TIERS:</li>
+        </ul>
+
+        <ul class="col_half left">
+            <li>$100.00</li>
+            <li>12345</li>
+            <li>Chocolate</li>
+            <li>13"</li>
+            <li>Circle</li>
+            <li>4-6</li>
+            <li>2</li>
+        </ul>
+        <div class="clr"></div>
+        <br />
+
+        <ul class="col_half left">
+            <li>PRINTED IMAGE:</li>
+            <li>DELIVERY:</li>
+            <li>OTHER SURCHARGE:</li>
+            <li>DISCOUNT:</li>
+        </ul>
+
+        <ul class="col_half left">
+            <li>$10.00</li>
+            <li>$10.00</li>
+            <li>$10.00</li>
+            <li>($10.00)</li>
+        </ul>
+        <div class="clr"></div>
+        <br />
+        <hr />
+        <br />
+        <ul class="col_half left">
+            <li>TOTAL</li>
+        </ul>
+
+        <ul class="col_half left">
+            <li>$140.00</li>
+        </ul>
+    </div>
+
+</div>
---
+++ application/views/email/invoice_thurmal_view.php
@@ -0,0 +1,259 @@
+<style type="text/css">
+    *
+    {
+        margin:0;
+        padding:0;
+        line-height:16px;
+        font-size:12px;
+        font-family:Times;
+    }
+    .clr
+    {
+        clear:both;
+    }
+    .cen
+    {
+        text-align:center;
+    }
+    #wrapper
+    {
+        width:350px;
+        margin:0 auto;
+    }
+    hr
+    {
+        margin:2px 0;
+    }
+    ul{
+        list-style-type:none;
+    }
+    h1
+    {
+        font-size:32px;
+        line-height:40px;
+        letter-spacing:5px;
+    }
+    .col_1
+    {
+        float:left;
+        width:50%;
+    }
+    .col_2
+    {
+        float:right;
+        width:50%;
+    }
+</style>
+<div id="wrapper">
+<h1 class="cen">ORDER</h1>
+<br />
+
+<p class="cen">St. Phillip's Bakery</p>
+<hr />
+<p class="cen">$store_addresse</p>
+<p class="cen">$store_city, $store_province $store_postal</p>
+<p class="cen">$store_email</p>
+<p class="cen">$store_phone
+
+</p>
+<br />
+
+<p>ORDER #$order_UID $order_date_dd/mm/yyyy</p>
+<hr />
+
+<ul class="col_1">
+    <li>
+        <p>CUSTOMER DETAILS</p>
+    </li>
+</ul>
+
+<ul class="col_2">
+    <li>
+        <p>DELIVERY DETAILS</p>
+    </li>
+</ul>
+<div class="clr"></div>
+<br />
+
+<ul class="col_1">
+    <li>$customer_first $customer_last</li>
+    <li>$customer_address_1</li>
+    <li>$customer_address_2</li>
+    <li>$customer_city, $customer_province $customer_postal</li>
+    <li>$customer_phone</li>
+    <li>$customer_email</li>
+</ul>
+
+<ul class="col_2">
+    <li>$location_or_contact_name</li>
+    <li>$address_1</li>
+    <li>$address_2</li>
+    <li>$city, $province $postal</li>
+    <li>$phone</li>
+    <li>$special_instructions</li>
+</ul>
+<div class="clr"></div>
+<br />
+
+<hr />
+<p>ORDER INFORMATION</p>
+<br />
+
+<ul class="col_1">
+    <li>
+        <p>PICKUP/DELIVERY:</p>
+    </li>
+    <li>
+        <p>DATE:</p>
+    </li>
+    <li>
+        <p>PICKUP LOCATION:</p>
+    </li>
+    <li>
+        <p>DELIVERY ZONE:</p>
+    </li>
+</ul>
+
+<ul class="col_2">
+    <li>
+        <p>$pickup_or_delivery</p>
+    </li>
+    <li>
+        <p>$pickup/delivery_date_dd/mm/yyyy $pickup/delivery_hour_24hr</p>
+    </li>
+    <li>
+        <p>$pickup_store_address $pickup_store_city, $pickup_store_postal</p>
+    </li>
+    <li>
+        <p>$delivery_zone</p>
+    </li>
+</ul>
+<div class="clr"></div>
+
+<hr />
+<p>CAKE DETAILS</p>
+<br />
+<p>IMAGE ON CAKE: $add_an_image_on_cake_filename</p>
+<p>REFERENCE PHOTOS: $add_instructional_photo_filename, $add_instructional_photo_filename, $etc</p>
+<br />
+<p>INSCRIPTION</p>
+<br />
+<p>> $inscriptionp>
+    <br />
+<p>SPECIAL INSTRUCTIONS</p>
+<br />
+<p>> $special_instructions</p>
+<p>> $special_instructions_continued</p>
+<hr />
+<ul class="col_1">
+    <li>
+        <p>Birthday Cake</p>
+    </li>
+    <li>
+        <p>MAGIC CAKE ID:</p>
+    </li>
+    <li>
+        <p>FLAVOUR:</p>
+    </li>
+    <li>
+        <p>SIZE:</p>
+    </li>
+    <li>
+        <p>SHAPE:</p>
+    </li>
+    <li>
+        <p>SERVING:</p>
+    </li>
+    <li>
+        <p>TIERS:</p>
+    </li>
+</ul>
+
+<ul class="col_2">
+    <li>
+        <p>$catalogue_price</p>
+    </li>
+    <li>
+        <p>$magic_cake_id_value</p>
+    </li>
+    <li>
+        <p>$cake_falvor_falue</p>
+    </li>
+    <li>
+        <p>$cake_size_value</p>
+    </li>
+    <li>
+        <p>$cake_shape_value</p>
+    </li>
+    <li>
+        <p>$cake_serving_value</p>
+    </li>
+    <li>
+        <p>$cake_tiers_value</p>
+    </li>
+</ul>
+<div class="clr"></div>
+
+
+<ul class="col_1">
+    <li>
+        <p>PRINTED IMAGE:</p>
+    </li>
+    <br />
+    <li>
+        <p>DELIVERY:</p>
+    </li>
+    <li>
+        <p>OTHER SURCHARGE:</p>
+    </li>
+    <li>
+        <p>DISCOUNT:</p>
+    </li>
+</ul>
+
+<ul class="col_2">
+    <li>
+        <p>$price_for_printed_image</p>
+    </li>
+    <br />
+    <li>
+        <p>$delivery_charge</p>
+    </li>
+    <li>
+        <p>$10.00</p>
+    </li>
+    <li>
+        <p>($managere_discount)</p>
+    </li>
+</ul>
+<div class="clr"></div>
+
+
+<hr />
+
+<ul class="col_1">
+    <li>
+        <p>TOTAL:</p>
+    </li>
+</ul>
+
+<ul class="col_2">
+    <li>
+        <p>$sum_of_above_values_or_manager_override</p>
+    </li>
+</ul>
+<div class="clr"></div>
+
+<br />
+<hr />
+<br />
+<p class="cen">Thank You</p>
+<p class="cen">$store-email</p>
+<p class="cen">stphillipsbakery.com</p>
+<br />
+
+<p class="cen">
+    <img src="http://gsipreview.net/stp-templates/img/barcode.jpg" width="60%"/>
+</p>
+
+</div>
