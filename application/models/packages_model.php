7060429d8dcd6bd5867a86e4037f8c872edf0aff
M Shafiq Islam <shafiq@emicrograph.com>
2013-07-25 16:21:08 +0600

    Ticket Resolved

--- application/models/packages_model.php
+++ application/models/packages_model.php
@@ -12,7 +12,7 @@ class Packages_model extends Ci_Model
 
     public function getAll($location=0){
 
-        $sql = ("SELECT price_matrix.price_matrix_id,price_matrix.price,servings.title AS servings_title , servings.size,flavours.*
+        $sql = ("SELECT price_matrix.price_matrix_id,price_matrix.price,servings.title AS servings_title , servings.printing_surcharge , servings.size,flavours.*
             FROM price_matrix
             LEFT JOIN servings ON price_matrix.serving_id = servings.serving_id
             LEFT JOIN flavours ON price_matrix.flavour_id = flavours.flavour_id WHERE location_id=$location"
@@ -25,6 +25,7 @@ class Packages_model extends Ci_Model
         foreach ($data as $key => $val) {
             $flavour_id = $val['flavour_id'];
             $price = (float) $val['price'];
+            $printing_surcharge = (float) $val['printing_surcharge'];
 
             if (!isset($flavours[$flavour_id])) {
                 $flavours[$flavour_id] = count($flavours);
@@ -37,6 +38,7 @@ class Packages_model extends Ci_Model
                 $output[$flavours[$flavour_id]]["servings"][$count]['price_matrix_id'] = (int) $val['price_matrix_id'];
                 $output[$flavours[$flavour_id]]["servings"][$count]['title'] = $val['servings_title'];
                 $output[$flavours[$flavour_id]]["servings"][$count]['size'] = $val['size'];
+                $output[$flavours[$flavour_id]]["servings"][$count]['printing_surcharge'] = $printing_surcharge;
                 $output[$flavours[$flavour_id]]["servings"][$count]['price'] = $price;
             }
 
