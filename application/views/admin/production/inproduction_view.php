6db8577f0a000c1a4bf885edb3a9dd264a1de2d5
M Shafiq Islam <shafiq@emicrograph.com>
2013-07-30 14:47:46 +0600

    Bug Fixing

--- application/views/admin/production/inproduction_view.php
+++ application/views/admin/production/inproduction_view.php
@@ -23,13 +23,7 @@
             success: function(val){
                 $('#filterResult').html(val);
                 $('#beforloading').html('');
-                $('.dynamicTable').dataTable({
-                    "sPaginationType": "bootstrap",
-                    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-                    "oLanguage": {
-                        "sLengthMenu": "_MENU_ records per page"
-                    }
-                });
+
                 //console.log(val);
             }
         })
--- application/views/admin/production/order_filtering_view.php
+++ application/views/admin/production/order_filtering_view.php
@@ -1,6 +1,6 @@
 <div class="widget">
     <div class="widget-body">
-        <table class="dynamicTable table table-striped table-bordered">
+        <table class="table table-striped table-bordered order-sorting">
             <thead>
             <tr>
                 <th width="70">Order #</th>
@@ -23,7 +23,7 @@
                     <td class="center"><a href="/admin/productions/details/<?php echo $rows->order_code; ?>" ><?php echo $rows->order_code; ?></a></td>
                     <td class="center"><?php echo $rows->first_name.' '.$rows->last_name; ?></td>
                     <td><?php echo $rows->cake_name; ?></td>
-                    <td class="center"><?php echo $rows->delivery_type; ?></td>
+                    <td class="center"><?php echo sentence_case($rows->delivery_type); ?></td>
                     <td class="center"><?php echo $this->productions_model->dateFormate($rows->delivery_date); ?></td>
                     <td class="center"><?php echo $rows->delivery_time; ?></td>
                     <td><?php if($rows->fondant >0){echo "Yes"; }else{ echo "No"; }; ?></td>
@@ -44,3 +44,47 @@
     <?php //echo $paging[1]; ?>
 
 </div>
+<script type="text/javascript" language="javascript" src="<?php echo base_url() ?>/assets/media/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf-8">
+    $(document).ready(function() {
+        jQuery.fn.dataTableExt.oSort['date-dd-mmm-yyyy-asc'] = function (a, b) {
+            "use strict"; //let's avoid tom-foolery in this function
+            var ordA = customDateDDMMMYYYYToOrd(a),
+                ordB = customDateDDMMMYYYYToOrd(b);
+            return (ordA < ordB) ? -1 : ((ordA > ordB) ? 1 : 0);
+        };
+
+        jQuery.fn.dataTableExt.oSort['date-dd-mmm-yyyy-desc'] = function (a, b) {
+            "use strict"; //let's avoid tom-foolery in this function
+            var ordA = customDateDDMMMYYYYToOrd(a),
+                ordB = customDateDDMMMYYYYToOrd(b);
+            return (ordA < ordB) ? -1 : ((ordA > ordB) ? 1 : 0);
+        };
+
+    } );
+    $(document).ready(function() {
+
+        $('.order-sorting').dataTable( {
+            "aaSorting": [[0,'desc']],
+            "aoColumns": [
+                null,
+                null,
+                null,
+                null,
+                {"sTYpe": "date-dd-mmm-yyyy" },
+                null,
+                null,
+                null,
+                null,
+                null
+            ]
+            /*"aoColumnDefs": [
+             { "bSortable":false, "aTargets": [2,3] }
+
+             ]*/
+        } );
+
+    } );
+
+</script>
+
