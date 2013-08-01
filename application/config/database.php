209007b67edede7ad9528721055c7ac528c42267
M Shafiq Islam <shafiq@emicrograph.com>
2013-08-01 19:52:21 +0600

    Instructionall Photo & Order Error Ticket

--- application/config/database.php
+++ application/config/database.php
@@ -47,8 +47,8 @@
 
 $active_group = 'default';
 $active_record = TRUE;
-
-/*$db['default']['hostname'] = 'localhost';
+/*
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = 'dhaka1';
 $db['default']['database'] = 'phillips';
@@ -82,7 +82,7 @@ $db['default']['stricton'] = FALSE;*/
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'commonrbs';
+$db['default']['password'] = '62O4jMyJcHlY';
 $db['default']['database'] = 'phillips';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
