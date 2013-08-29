5f681f7bf78c9d71e2d2d1a85420c3d32aae5e82
M Shafiq Islam <shafiq@emicrograph.com>
2013-08-29 15:54:48 +0600

    Blackout Multiple Flavour

--- application/models/blackouts_model.php
+++ application/models/blackouts_model.php
@@ -18,44 +18,49 @@ class Blackouts_model extends Crud_Model
         $curdate=date('m/d/Y');
 
         $location_id=$this->input->post('location_id');
-        $flavour_id=$this->input->post('flavour_id');
-        $dbBlack=$this->getBlackouts($location_id,$flavour_id);
-        $dbblackout = isset($dbBlack[0]->blackout_id)? $dbBlack[0]->blackout_id :0;
-        if (!$dbblackout > 0 ){
+        $flavour=$this->input->post('flavour_id');
 
-            $blackout_date      = explode(',',$this->input->post('blackout_date'));
-            $final          = array();
+        foreach($flavour as $flavour_id):
 
-            foreach($blackout_date as $date){
+            $dbBlack=$this->getBlackouts($location_id,$flavour_id);
+            $dbblackout = isset($dbBlack[0]->blackout_id)? $dbBlack[0]->blackout_id :0;
 
-                if($date >= $curdate){
+            if (!$dbblackout > 0 ){
 
-                    $final[]=$date;
+                $blackout_date      = explode(',',$this->input->post('blackout_date'));
+                $final          = array();
+
+                foreach($blackout_date as $date){
+
+                    if($date >= $curdate){
+
+                        $final[]=$date;
+                    }
                 }
-            }
-            $final_date=implode(',',$final);
-            $this->db->set(array('location_id'=>$location_id , 'flavour_id'=>$this->input->post('flavour_id'),'blackout_date'=>$final_date))->insert('blackouts');
+                $final_date=implode(',',$final);
+                $this->db->set(array('location_id'=>$location_id , 'flavour_id'=>$flavour_id,'blackout_date'=>$final_date))->insert('blackouts');
 
-        }else{
+            }else{
 
 
-            $blackout_date  = explode(',',$dbBlack[0]->blackout_date) ;
-            $beginning      = explode(',',$this->input->post('blackout_date'));
-            $merge          = array_merge((array)$beginning, (array)$blackout_date);
-            $unique         = array_unique($merge);
-            $final          = array();
+                $blackout_date  = explode(',',$dbBlack[0]->blackout_date) ;
+                $beginning      = explode(',',$this->input->post('blackout_date'));
+                $merge          = array_merge((array)$beginning, (array)$blackout_date);
+                $unique         = array_unique($merge);
+                $final          = array();
 
-            foreach($unique as $date){
+                foreach($unique as $date){
 
-                if($date >= $curdate){
+                    if($date >= $curdate){
 
-                    $final[]=$date;
+                        $final[]=$date;
+                    }
                 }
+                $final_date=implode(',',$final);
+                $this->db->set(array('location_id'=>$location_id ,'flavour_id'=>$flavour_id,'blackout_date'=>$final_date))->where('blackout_id',$dbBlack[0]->blackout_id)->update('blackouts');
             }
-            $final_date=implode(',',$final);
-            $this->db->set(array('location_id'=>$location_id ,'flavour_id'=>$this->input->post('flavour_id'),'blackout_date'=>$final_date))->where('blackout_id',$dbBlack[0]->blackout_id)->update('blackouts');
-        }
 
+        endforeach;
     }
 
 
