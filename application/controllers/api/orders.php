683a1e9126ab5acb6ac52e14de62d2edcf31afaa
M Shafiq Islam <shafiq@emicrograph.com>
2013-09-10 18:39:37 +0600

    Revel Updated

--- application/controllers/api/orders.php
+++ application/controllers/api/orders.php
@@ -264,18 +264,13 @@ class Orders extends API_Controller
 
 
         $revel_order_id = $this->revel_order->getRevelID('orders', $orders['order_id']);
-
-
         if(empty($revel_order_id) && $orders['order_status'] != '300' ){
 
-
-            $revel_product = $this->revel_order->getRevelID('cakes',$orders['cake_id']);
             $revel_customer = $this->revel_order->getRevelID('customers',$orders['customer_id']);
             $revel_location = $this->revel_order->getRevelID('locations',$orders['location_id']);
 
             $RevelOrderData = array(
                 'order_code' => $orders['order_code'],
-                'revel_product_id' =>  $revel_product,
                 'revel_customer_id' => $revel_customer,
                 'revel_location_id' => $revel_location,
                 'discount'=> $orders['discount_price'],
@@ -283,7 +278,10 @@ class Orders extends API_Controller
             );
 
            try{
-               $status_code_revel =  $this->revel_order->create($RevelOrderData);
+
+               $custom =( $orders['cake_id'] > 0 ) ? $orders['cake_id'] :'';
+               $status_code_revel =  $this->revel_order->create($RevelOrderData,$custom);
+
                $orders['revel_order_id']  = $status_code_revel;
            } catch (\Exception $e){
                $orders['revel_order_id'] = null;
@@ -296,14 +294,14 @@ class Orders extends API_Controller
             }
 
          }else{
+
             if($revel_order_id){
-                $revel_product = $this->revel_order->getRevelID('cakes',$orders['cake_id']);
+
                 $revel_customer = $this->revel_order->getRevelID('customers',$orders['customer_id']);
                 $revel_location = $this->revel_order->getRevelID('locations',$orders['location_id']);
                 $RevelOrderData = array(
                     'order_code' => $orders['order_code'],
                     'revel_order_id' => $revel_order_id,
-                    'revel_product_id' =>  $revel_product,
                     'revel_customer_id' => $revel_customer,
                     'revel_location_id' => $revel_location,
                     'discount'=> $orders['discount_price'],
