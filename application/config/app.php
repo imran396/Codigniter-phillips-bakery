eb2eabacd6d803d5c8a80dd8f443643095c638a4
Emran Hasan <phpfour@gmail.com>
2013-09-10 16:40:44 +0600

    Revel integration - added default product reference for custom/catalog cakes.

--- application/config/app.php
+++ application/config/app.php
@@ -6,4 +6,7 @@ $config['revel']['api_key']    = '99a29a4b65fa49138849f39a9e90fdf9';
 $config['revel']['api_secret'] = '65409fdecef64defaee3ef130982cff17813cc98b38d47fd95ed168ccb306c39';
 
 // Default Data
+$config['revel']['user']          = '/enterprise/User/1/';
 $config['revel']['establishment'] = '/enterprise/Establishment/1/';
+$config['revel']['customCake']    = "/resources/Product/1/";
+$config['revel']['catalogCake']   = "/resources/Product/2/";
