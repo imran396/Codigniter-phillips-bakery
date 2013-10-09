04a834db8732947c8f3c5d33f5be5520aaaff5e1
M Shafiq Islam <shafiq@emicrograph.com>
2013-10-09 16:00:04 +0600

    Revel Update

--- application/config/app.php
+++ application/config/app.php
@@ -5,16 +5,16 @@
 // This code use for live site
 
 
-$config['revel']['endpoint']   = 'http://stphillips.revelup.com';
+/*$config['revel']['endpoint']   = 'http://stphillips.revelup.com';
 $config['revel']['api_key']    = '57a86885d3bf4e409f495b504aad33c9';
-$config['revel']['api_secret'] = 'f500e9593583437683be1127f0cf7a434d0901f965824bb2b74b01897dafef44';
+$config['revel']['api_secret'] = 'f500e9593583437683be1127f0cf7a434d0901f965824bb2b74b01897dafef44';*/
 
 
 
 // This code use for dev site
-/*$config['revel']['endpoint']   = 'https://teststphilips.revelup.com';
+$config['revel']['endpoint']   = 'https://teststphilips.revelup.com';
 $config['revel']['api_key']    = '99a29a4b65fa49138849f39a9e90fdf9';
-$config['revel']['api_secret'] = '65409fdecef64defaee3ef130982cff17813cc98b38d47fd95ed168ccb306c39';*/
+$config['revel']['api_secret'] = '65409fdecef64defaee3ef130982cff17813cc98b38d47fd95ed168ccb306c39';
 
 // Default Data
 $config['revel']['user']          = '/enterprise/User/1/';
