6862ac22f780374c2f7f88d85641af1dd1d7832e
shafiq <shafiqabs@gmail.com>
2013-10-29 15:01:37 +0600

    Menu level add reports menu

--- application/views/admin/layouts/left.php
+++ application/views/admin/layouts/left.php
@@ -44,7 +44,17 @@ $array = array('roles','controls','access_controls');
                 </ul>
             </li>
             <li class="glyphicons cogwheels <?php echo (!empty($active) && $active == 'auditlog') ? 'active' : ''; ?>"><a href="/admin/auditlog/listing"><i></i><span><?php echo $this->lang->line('auditlog'); ?></span></a></li>
+            <li class="hasSubmenu <?php echo (!empty($active) && in_array($active,$array) ) ? 'active' : ''; ?>">
+                <a data-toggle="collapse " class="glyphicons show_thumbnails_with_lines " href="#menu_access"><i></i><span><?php echo $this->lang->line('reports'); ?></span></a>
+                <ul class="collapse <?php echo (!empty($active) && in_array($active,$array )) ? 'in' : ''; ?>" id="menu_access">
 
+                    <li class=""><a href="/admin/reports/product_reports"><span><?php echo $this->lang->line('product_reports'); ?></span></a></li>
+                    <li class=""><a href="/admin/reports/customer_reports"><span><?php echo $this->lang->line('customer_reports'); ?></span></a></li>
+                    <li class=""><a href="/admin/reports/category_reports"><span><?php echo $this->lang->line('category_reports'); ?></span></a></li>
+
+
+                </ul>
+            </li>
         </ul>
 
     </div>
