03bc09dc95fdd0bc5144a8807b722d89c2fcf6a5
shafiq <shafiqabs@gmail.com>
2013-11-03 15:04:26 +0600

    Cake search paging update function.

--- application/language/english/english_lang.php
+++ application/language/english/english_lang.php
@@ -179,7 +179,7 @@ $lang['fondant'] 		 	        = 'Fondant';
 $lang['add_servings'] 		 	    = 'Add Servings';
 $lang['serving_name'] 		 	    = 'Number of Servings';
 $lang['size'] 		 	            = 'Size';
-$lang['size_shape'] 		 	    = 'Size & Shape';
+$lang['size_shape'] 		 	    = 'Shape';
 $lang['printing_surcharge'] 		 	    = 'Printing Surcharge';
 
 /* price matrix */
@@ -306,8 +306,8 @@ $lang['auditlog'] = 'Audit Log';
 /*-------------------Reports----------------------*/
 
 $lang['reports'] 		 	    = 'Reports';
-$lang['product_reports'] 		 	    = 'Product Reports';
-$lang['category_reports'] 		 	    = 'Category Reports';
-$lang['customer_reports'] 		 	    = 'Customer Reports';
+$lang['product_reports'] 		 	    = 'Product report';
+$lang['category_reports'] 		 	    = 'Category report';
+$lang['customer_reports'] 		 	    = 'Customer report';
 $lang['enter_start_date'] 		 	    = 'Enter start date[mm/dd/yyyyy]';
 $lang['enter_end_date'] 		 	    = 'Enter end date[mm/dd/yyyy]';
