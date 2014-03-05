5d4875f6bb61fe8cb6b034ad1083e7c70ff17d85
shafiq <shafiqabs@gmail.com>
2014-03-05 19:35:42 +0600

    Add Revel API KEY

---
+++ application/config/app.php
@@ -0,0 +1,36 @@
+<?php
+
+// Service Information
+
+// This code use for live site
+
+
+
+// Live Server
+
+$config['revel']['endpoint']   = 'https://stphillips.revelup.com';
+$config['revel']['api_key']    = 'a17d6fad3bd748aaa7f402b0bc38ad4c';
+$config['revel']['api_secret'] = 'ea7f382f8c10409ca917b91068af830ebd83185bfb90483d834cb66e8687e2c1';
+
+$config['revel']['user']          = '/enterprise/User/1/';
+$config['revel']['establishment'] = '/enterprise/Establishment/1/';
+$config['revel']['customCake']    = "/resources/Product/2125/";
+$config['revel']['catalogCake']   = "/resources/Product/2126/";
+
+
+
+
+// This code use for dev site
+
+// Dev Server
+
+/*$config['revel']['endpoint']   = 'https://teststphilips.revelup.com';
+$config['revel']['api_key']    = '99a29a4b65fa49138849f39a9e90fdf9';
+$config['revel']['api_secret'] = '65409fdecef64defaee3ef130982cff17813cc98b38d47fd95ed168ccb306c39';
+
+// Default Data
+$config['revel']['user']          = '/enterprise/User/1/';
+$config['revel']['establishment'] = '/enterprise/Establishment/2/';
+
+$config['revel']['customCake']    = "/resources/Product/1/";
+$config['revel']['catalogCake']   = "/resources/Product/2/";*/
