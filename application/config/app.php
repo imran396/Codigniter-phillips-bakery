3272ea6dc93b3a5adc98cfcba9e296a26e493244
shafiq <shafiqabs@gmail.com>
2014-04-06 12:20:00 +0600

    App config file upload.

--- application/config/app.php
+++ application/config/app.php
@@ -8,14 +8,14 @@
 
 // Live Server
 
-$config['revel']['endpoint']   = 'https://stphillips.revelup.com';
+/*$config['revel']['endpoint']   = 'https://stphillips.revelup.com';
 $config['revel']['api_key']    = 'a17d6fad3bd748aaa7f402b0bc38ad4c';
 $config['revel']['api_secret'] = 'ea7f382f8c10409ca917b91068af830ebd83185bfb90483d834cb66e8687e2c1';
 
 $config['revel']['user']          = '/enterprise/User/1/';
 $config['revel']['establishment'] = '/enterprise/Establishment/1/';
 $config['revel']['customCake']    = "/resources/Product/2125/";
-$config['revel']['catalogCake']   = "/resources/Product/2126/";
+$config['revel']['catalogCake']   = "/resources/Product/2126/";*/
 
 
 
@@ -24,7 +24,7 @@ $config['revel']['catalogCake']   = "/resources/Product/2126/";
 
 // Dev Server
 
-/*$config['revel']['endpoint']   = 'https://teststphilips.revelup.com';
+$config['revel']['endpoint']   = 'https://teststphilips.revelup.com';
 $config['revel']['api_key']    = '99a29a4b65fa49138849f39a9e90fdf9';
 $config['revel']['api_secret'] = '65409fdecef64defaee3ef130982cff17813cc98b38d47fd95ed168ccb306c39';
 
@@ -33,4 +33,4 @@ $config['revel']['user']          = '/enterprise/User/1/';
 $config['revel']['establishment'] = '/enterprise/Establishment/2/';
 
 $config['revel']['customCake']    = "/resources/Product/1/";
-$config['revel']['catalogCake']   = "/resources/Product/2/";*/
+$config['revel']['catalogCake']   = "/resources/Product/2/";
